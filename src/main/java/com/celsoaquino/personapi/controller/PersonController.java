00d6ef43f20d41ecd1d9871e8ccf77225feea68b
celsoaquino <celso.aquino@ymail.com>
2021-03-03 18:02:46 -0300

    Improved person creation functionality

--- src/main/java/com/celsoaquino/personapi/controller/PersonController.java
+++ src/main/java/com/celsoaquino/personapi/controller/PersonController.java
@@ -1,19 +1,17 @@
 package com.celsoaquino.personapi.controller;
 
 import com.celsoaquino.personapi.dto.MessageResponseDTO;
-import com.celsoaquino.personapi.entity.Person;
-import com.celsoaquino.personapi.repository.PersonRepository;
+import com.celsoaquino.personapi.dto.request.PersonDTO;
 import com.celsoaquino.personapi.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+
 @RestController
 @RequestMapping("/api/v1/people")
 public class PersonController {
-
-    @Autowired
-    PersonRepository personRepository;
 
     private PersonService personService;
 
@@ -24,11 +22,7 @@ public class PersonController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public MessageResponseDTO createPerson(@RequestBody Person person) {
-        Person savedPerson = personRepository.save(person);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID " + savedPerson.getId())
-                .build();
+    public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO) {
+        return personService.createPerson(personDTO);
     }
 }
--- src/main/java/com/celsoaquino/personapi/mapper/PersonMapper.java
+++ src/main/java/com/celsoaquino/personapi/mapper/PersonMapper.java
@@ -3,6 +3,7 @@ package com.celsoaquino.personapi.mapper;
 import com.celsoaquino.personapi.dto.request.PersonDTO;
 import com.celsoaquino.personapi.entity.Person;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
@@ -10,7 +11,7 @@ public interface PersonMapper {
 
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    //@Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
+    @Mapping(target = "birthDate", source = "birthDate", dateFormat = "dd-MM-yyyy")
     Person toModel(PersonDTO personDTO);
 
     PersonDTO toDTO(Person person);
